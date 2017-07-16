1fab10395c8c91a0c833fb61b9abe20a6c1bd084
Utphala <utphala@utphalas-air.lan>
2017-07-15 20:01:17 -0700

    Some chagnes for google auth etc

--- webApp.py
+++ webApp.py
@@ -8,18 +8,18 @@ import json
 
 GOOGLE_CLIENT_ID = '37779483905-mbehq3u75fg4ldo9jb3t9pg5t1ck83c2.apps.googleusercontent.com'
 GOOGLE_CLIENT_SECRET = 'DOf9Z2tv3mMkBvOOTyeroC-F'
-REDIRECT_URI = '/list_surveys'
+REDIRECT_URI = '/authorize'
 
 app = Flask(__name__, template_folder='templates')
 
 SECRET_KEY = 'development key'
 DEBUG = True
- 
+
 app = Flask(__name__)
 app.debug = DEBUG
 app.secret_key = SECRET_KEY
 oauth = OAuth()
- 
+
 google = oauth.remote_app('google',
                           base_url='https://www.google.com/accounts/',
                           authorize_url='https://accounts.google.com/o/oauth2/auth',
@@ -31,16 +31,16 @@ google = oauth.remote_app('google',
                           access_token_params={'grant_type': 'authorization_code'},
                           consumer_key=GOOGLE_CLIENT_ID,
                           consumer_secret=GOOGLE_CLIENT_SECRET)
- 
+
 @app.route('/')
 def index():
     access_token = session.get('access_token')
     if access_token is None:
         return redirect(url_for('login'))
- 
+
     access_token = access_token[0]
     from urllib2 import Request, urlopen, URLError
- 
+
     headers = {'Authorization': 'OAuth '+access_token}
     req = Request('https://www.googleapis.com/oauth2/v1/userinfo',
                   None, headers)
@@ -52,53 +52,64 @@ def index():
             session.pop('access_token', None)
             return redirect(url_for('login'))
         return res.read()
-    #print res['email']
-    return json.dumps(res)
- 
- 
+    # Save user data
+    data = json.load(res)
+    loadUser(data)
+    return res.read()
+
+
 @app.route('/login')
 def login():
     callback=url_for('authorized', _external=True)
     return google.authorize(callback=callback)
- 
- 
- 
+
+
+
 @app.route(REDIRECT_URI)
 @google.authorized_handler
 def authorized(resp):
     access_token = resp['access_token']
     session['access_token'] = access_token, ''
-    return redirect(url_for('index'))
- 
- 
+    return redirect(url_for('list_surveys'))
+
+
 @google.tokengetter
 def get_access_token():
     return session.get('access_token')
 
+def loadUser(data):
+    conn=MySQLdb.connect('127.0.0.1','root','root','youTellMeDB')
+    cursor= conn.cursor()
+    cursor.execute("INSERT IGNORE INTO youTellMeDB.Users (id, role, name) VALUES (%s, %s, %s)", (data['email'], 'user', data['name']));
+    conn.commit()
+    cursor.close()
+    conn.close()
 
 @app.route("/list_surveys",methods=['GET','POST'])
-def list_surveys():
+@google.authorized_handler
+def list_surveys(resp):
     query_to_excute = "SELECT content FROM youTellMeDB.surveys";
     conn=MySQLdb.connect('127.0.0.1','root','root','youTellMeDB')
-    pointer= conn.cursor()
-    pointer.execute(query_to_excute);
-    surveys = pointer.fetchall()
-    print surveys
-    pointer.close()
+    cursor= conn.cursor()
+    cursor.execute(query_to_excute);
+    surveys = cursor.fetchall()
+    cursor.close()
     conn.close()
     return jsonify(surveys)
 
-@app.route("/get_survey", methods=["POST"])
-def get_survey():
-    json_dict = request.get_json()
-    title = json_dict['Title']
-    data = {"Title": title}
-    return jsonify(data)
+@app.route("/get_survey/<sid>", methods=["GET", "POST"])
+def get_survey(sid):
+    query_to_excute = "SELECT content FROM youTellMeDB.surveys WHERE id=%s";
+    conn=MySQLdb.connect('127.0.0.1','root','root','youTellMeDB')
+    cursor= conn.cursor()
+    cursor.execute(query_to_excute,sid);
+    survey = cursor.fetchone()
+    return json.dumps(survey)
 
-@app.route("/submit", methods=["POST"])
+@app.route("/submit_survey", methods=["POST"])
 def submit():
     return
 
 
 if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=8081)
+    app.run(host='0.0.0.0', port=8080)
